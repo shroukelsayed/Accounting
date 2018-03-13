d9e737c33374d6bed3044393bae41b0ad24f7b2c
shroukelsayed <shrouk.passiony@gmail.com>
2018-03-13 15:30:55 +0200

    pgsql database

---
+++ database/seeds/RoleTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class RoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+         DB::table('roles')->insert([
+            'id' => 1,
+            'role_name' => 'Admin',
+            'role' => 1,
+        ]);
+    }
+}
