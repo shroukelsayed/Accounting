89fb91cf254d74710339b66719185cba73268caa
shroukelsayed <shrouk.passiony@gmail.com>
2018-05-06 10:35:53 +0200

    level one relation

--- database/migrations/2018_03_26_153832_create_accounting_tree_level_ones_table.php
+++ database/migrations/2018_03_26_153832_create_accounting_tree_level_ones_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateAccountingTreeLevelOnesTable extends Migration
+class CreateAccountingTreeLevelOneTable extends Migration
 {
     /**
      * Run the migrations.
