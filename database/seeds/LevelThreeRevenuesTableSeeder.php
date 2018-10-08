ef07b4490c278da01e89774431e2dd3ccbd8014c
shroukelsayed <shrouk.passiony@gmail.com>
2018-10-08 14:40:52 +0200

    seeders

---
+++ database/seeds/LevelThreeRevenuesTableSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+
+use Laracasts\TestDummy\Factory as TestDummy;
+
+class LevelThreeRevenuesTableSeeder extends Seeder {
+
+    public function run()
+    {
+        DB::table('level_three_revenues')->insert([
+        	'id' => '13',
+        	'code' => '4101',
+        	'title' => 'ايرادات  فوائد الودائع البنك التجاري الدولي ',
+        	'parent' => '6',
+        	'level' => '3',
+        	'debit' => '1',
+        	'credit' => '0',
+        	'created_at' => '2018-07-19 14:47:16',
+        	'updated_at' => '2018-07-19 14:47:16',
+        ]);
+        DB::table('level_three_revenues')->insert([
+        	'id' => '15',
+        	'code' => '4102',
+        	'title' => 'ايرادات  فوائد الوائع البنك المصرف المتحد ',
+        	'parent' => '6',
+        	'level' => '3',
+        	'debit' => '0',
+        	'credit' => '1',
+        	'created_at' => '2018-07-19 15:03:59',
+        	'updated_at' => '2018-07-19 15:03:59',
+        ]);
+    }
+
+}
+
+
