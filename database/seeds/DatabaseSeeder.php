9cb41d797d46f7f7b7290cad679d2a291978b2ca
shroukelsayed <shrouk.passiony@gmail.com>
2018-10-08 13:49:36 +0200

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,13 +11,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(RoleTableSeeder::class);
-        // $this->call(UserTableSeeder::class);
-        // $this->call(AccountingTreeLevelOneTableSeeder::class);
-        // $this->call(AccountingTreeLevelTwoTableSeeder::class);
-        // $this->call(CurrentAssetsTableSeeder::class);
-        // $this->call(CurrentLiabilitiesTableSeeder::class);
-        // $this->call(FixedAssetsTableSeeder::class);
+        $this->call(RoleTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        $this->call(AccountingTreeLevelOneTableSeeder::class);
+        $this->call(AccountingTreeLevelTwoTableSeeder::class);
+        $this->call(CurrentAssetsTableSeeder::class);
+        $this->call(CurrentLiabilitiesTableSeeder::class);
+        $this->call(FixedAssetsTableSeeder::class);
 
         $this->call(CustodyAndAdvancesTableSeeder::class);
         $this->call(FriendshipFundsTableSeeder::class);
